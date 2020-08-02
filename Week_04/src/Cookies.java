36c44c614ce833f4e1b80a65b07f0fb5c6f46868
CDL <desperadocui@163.com>
2020-08-02 21:57:45 +0800

    作业提交

---
+++ Week_04/src/Cookies.java
@@ -0,0 +1,17 @@
+/**
+ * 455. 分发饼干
+ * https://leetcode-cn.com/problems/assign-cookies/
+ */
+class Cookies {
+    public int findContentChildren(int[] g, int[] s) {
+        if(g == null || s == null) return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi = 0, si = 0;
+        while(gi < g.length && si < s.length){
+            if(g[gi] <= s[si]) gi++;
+            si++;
+        }
+        return gi;
+    }
+}
