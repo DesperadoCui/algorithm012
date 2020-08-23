4c05020b659a05182e3bc79d7c02adc03378d959
CDL <desperadocui@163.com>
2020-08-23 15:07:05 +0800

    作业提交

---
+++ Week_07/src/LadderWords.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+            return 0;
+        }
+
+        // 标准写法，总的 visited 数组
+        Set<String> visited = new HashSet<>();
+
+        Set<String> beginVisited = new HashSet<>();
+        beginVisited.add(beginWord);
+
+        Set<String> endVisited = new HashSet<>();
+        endVisited.add(endWord);
+
+        int len = beginWord.length();
+        int step = 1;
+        while (!beginVisited.isEmpty() && !endVisited.isEmpty()) {
+            if (beginVisited.size() > endVisited.size()) {
+                Set<String> temp = beginVisited;
+                beginVisited = endVisited;
+                endVisited = temp;
+            }
+
+            Set<String> nextLevelVisited = new HashSet<>();
+            for (String word : beginVisited) {
+                char[] charArray = word.toCharArray();
+                for (int i = 0; i < len; i++) {
+                    char originChar = charArray[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (originChar == c) {
+                            continue;
+                        }
+                        charArray[i] = c;
+                        String nextWord = String.valueOf(charArray);
+                        if (wordSet.contains(nextWord)) {
+                            if (endVisited.contains(nextWord)) {
+                                return step + 1;
+                            }
+                            if (!visited.contains(nextWord)) {
+                                nextLevelVisited.add(nextWord);
+                                visited.add(nextWord);
+                            }
+                        }
+                    }
+                    // 恢复
+                    charArray[i] = originChar;
+                }
+            }
+            beginVisited = nextLevelVisited;
+            step++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        List<String> wordList = new ArrayList<>();
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+        Collections.addAll(wordList, words);
+
+        Solution solution = new Solution();
+        String beginWord = "hit";
+        String endWord = "cog";
+        int ladderLength = solution.ladderLength(beginWord, endWord, wordList);
+        System.out.println(String.format("从 %s 到 %s 的最短转换序列的长度：%d。", beginWord, endWord, ladderLength));
+    }
+}
